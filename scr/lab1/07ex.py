b8af7561e7b2c6ac4a67f319e2e9af53e089defa
liza <yaremenkoelizaveta5@gmail.com>
2026-09-15 21:18:17 +0300

    67

--- scr/lab1/07ex.py
+++ scr/lab1/07ex.py
@@ -1,17 +1,17 @@
 s = input()
 res = ''
-ch = '0123456789'
+ch = '0123456789' # список для проверки является элементом числом или нет
 k = 0
 ind1 = 0
 ind2 = 0
 
-for i in range(len(s)):
+for i in range(len(s)): # цикл для нахождения первой заглавной буквы и добавления ее в список результата
     if s[i].isupper() == True:
         res += s[i]
         ind1 = i
         break
 
-for j in range(ind1, len(s)-1):
+for j in range(ind1, len(s)-1): # цикл, чтобы найти вторую буквы и узнать шаг, чтобы собрать сообщение до конца
     k += 1
     if s[j] in ch:
         res += s[j+1]
@@ -19,7 +19,7 @@ for j in range(ind1, len(s)-1):
         shag = abs(ind1 - ind2)
         break
 
-for e in range(ind2, len(s)-shag, shag):
+for e in range(ind2, len(s)-shag, shag): #цикл, чтобы закончить сообщение. начинаем со второго элемента и дальше с известным шагом
     res += s[e+shag]
 
 print(res)
--- scr/lab1/readme.md
+++ scr/lab1/readme.md
@@ -6,3 +6,45 @@
 ![alt text](../../images/lab1/01pr.png)
 запуск
 ![alt text](../../images/lab1/01z.png)
+
+2 задание
+
+программа
+![alt text](../../images/lab1/02pr.png)
+запуск
+![alt text](../../images/lab1/02z.png)
+
+3 задание
+
+программа
+![alt text](../../images/lab1/03pr.png)
+запуск
+![alt text](../../images/lab1/03z.png)
+
+4 задание
+
+программа
+![alt text](../../images/lab1/04pr.png)
+запуск
+![alt text](../../images/lab1/04z.png)
+
+5 задание
+
+программа
+![alt text](../../images/lab1/05pr.png)
+запуск
+![alt text](../../images/lab1/05z.png)
+
+6 задание
+
+программа
+![alt text](../../images/lab1/06pr.png)
+запуск
+![alt text](../../images/lab1/06z.png)
+
+7 задание
+
+программа
+![alt text](../../images/lab1/07pr.png)
+запуск
+![alt text](../../images/lab1/07z.png)
